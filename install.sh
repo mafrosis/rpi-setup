14b6d68c3a9464f8d3ce5b5a4ab7dcc1391278fe
Matt Black <github@mafro.net>
2022-11-22 20:33:16 +1100

    Fix the SSID password hashes; add logging

--- install.sh
+++ install.sh
@@ -13,23 +13,34 @@ PASS="$(echo -n "$PASS" | /opt/homebrew/opt/openssl/bin/openssl passwd -6 -stdin
 
 # set default user/pass
 echo "pi:${PASS}" > userconf.txt
+echo 'Written /boot/userconf.conf'
 
 # setup sshd
 touch ssh
+echo 'Created /boot/ssh'
 
-read -rp "Enter bacon wifi password: " PSK
+read -rp "Which SSID? 1. bacon, 2. eggs_iot " SSID
 
-# hash password
-PSK="$(echo -n "$PSK" | iconv -t UTF-16LE | openssl md4)"
+if [[ $SSID -eq 1 ]]; then
+	SSID='bacon'
+	PSK='7edb78e26f73b7b2bc9a401f3c8abdea6d2ee193c78f6e92335061c7b6fdb655'
+elif [[ $SSID -eq 2 ]]; then
+	SSID='eggs_iot'
+	PSK='8d0b47dc934183b58e86cb42599fe0979e2165cd78b5f2d9379bbae5d50e47db'
+else
+	echo 'Invalid choice, you lose'
+	exit 44
+fi
 
-# setup wifi
+# setup wpa_supplicant
 tee wpa_supplicant.conf > /dev/null <<EOF
-ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-update_config=1
+ctrl_interface=/run/wpa_supplicant
 country=AU
 
 network={
-  ssid="bacon"
-  psk="hash:${PSK}"
+  ssid="${SSID}"
+  psk=${PSK}
 }
 EOF
+
+echo 'Written /boot/wpa_supplicant.conf'
